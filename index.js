c465013fbd4e1b6378205e80342291e83594532b
Noé Leroi <nleroi.pro@gmail.com>
2023-10-24 14:24:47 +0200

    refactored download function and added /mp4 GET endpoint

--- index.js
+++ index.js
@@ -8,19 +8,28 @@ app.listen(4000, function(){
     console.log("listening on 4000");
 });
 
-app.get('/download', function(req, res) {
-    var link = req.query.url;
-
+function convertAndDownloadVideo(link, type, res) {
     // Extraire le nom de la vidéo de l'URL
     var videoInfo = ytdl.getBasicInfo(link);
     videoInfo.then(info => {
-
         const sanitizedTitle = info.videoDetails.title.replace(/\//g, '');
-        const fileName = sanitizedTitle + '.mp3'; 
-        const video = ytdl(link, { filter: 'audioonly' });
+        let fileName = ''
+        let video = ytdl(link, { filter: 'audioonly' });
 
+        if(type === 'mp3')
+        {
+            fileName = sanitizedTitle + '.mp3'; 
+            video = ytdl(link, { filter: 'audioonly' });
+        }
+        else{
+            fileName = sanitizedTitle + '.mp4'; 
+            video = ytdl(link, {
+                quality: 'highest',
+                filter: 'videoandaudio',
+                format: 'mp4',
+            });
+        }
         video.pipe(fs.createWriteStream(fileName));
-
         // Attendez que l'écriture soit terminée, puis renvoyez le fichier au client
         video.on('end', function () {
             res.download(fileName, function(err) {
@@ -41,4 +50,14 @@ app.get('/download', function(req, res) {
         console.error(error);
         res.status(500).send('Erreur lors de la récupération des informations de la vidéo.');
     });
+}
+
+app.get('/download/mp3', function(req, res) {
+    var link = req.query.url;
+    convertAndDownloadVideo(link, 'mp3', res)
+});
+
+app.get('/download/mp4', function(req, res) {
+    var link = req.query.url;
+    convertAndDownloadVideo(link, 'mp4', res)
 });
